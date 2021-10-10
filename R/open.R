315b76b83334c3b4077413f13bd0ca53b105b36c
Lluís <lluis.revilla@gmail.com>
2021-10-10 17:53:07 +0200

    Use pipe on 4.1 and simplify  call to database

--- R/open.R
+++ R/open.R
@@ -5,37 +5,35 @@ library("RSQLite")
 library("RMySQL")
 # Connecting R with MySQL
 open_bugs <- function() {
-    db_bugzilla <- dbConnect(RMySQL::MySQL(), dbname = "rbugs", user = "tester",
-                             password = "password-Tester1!",
-                             host = "127.0.0.1")
-    db_bugs <- tbl(db_bugzilla, "bugs") %>%
+    db_bugzilla <- dbConnect(RMySQL::MySQL(),  dbname = "rbugs")
+    db_bugs <- tbl(db_bugzilla, "bugs") |>
         filter(resolution == "")
 
-    bugs <- db_bugs %>%
-        collect() %>%
+    bugs <- db_bugs |>
+        collect() |>
         select(
             bug_id, bug_status, short_desc, version, component_id, rep_platform, op_sys, creation_ts, delta_ts
         )
 
-    db_comments <- db_bugzilla %>%
-        tbl("longdescs") %>%
+    db_comments <- db_bugzilla |>
+        tbl("longdescs") |>
         right_join(db_bugs)
     r_core <- c(3, 5, 9, 18, 19, 28, 34, 54, 137, 151, 216, 308, 413, 420, 1249,
                 1330, 2442)
-    r <- db_comments %>%
-        collect() %>%
-        group_by(bug_id) %>%
+    r <- db_comments |>
+        collect() |>
+        group_by(bug_id) |>
         summarize(r_core = ifelse(any(who %in% r_core), "yes", "no"),
                   total_comments = n() - 1,
                   people = sum(who != 2))
 
-    db_attachments <- db_bugzilla %>%
-        tbl("attachments") %>%
+    db_attachments <- db_bugzilla |>
+        tbl("attachments") |>
         right_join(db_bugs, by = "bug_id")
 
-    at <- db_attachments %>%
-        collect() %>%
-        group_by(bug_id) %>%
+    at <- db_attachments |>
+        collect() |>
+        group_by(bug_id) |>
         summarize(has_attachment = ifelse(any(!is.na(attach_id)), "yes", "no"))
     dbb <- full_join(r, bugs)
     dbb <- full_join(dbb, at)
@@ -64,15 +62,26 @@ open_bugs <- function() {
         warning("Please report it that some components aren't known")
     }
 
+    today <- Sys.Date()
+    tp <- db_bugzilla |>
+        tbl("bugs_activity") |>
+        collect() |>
+        filter(bug_id %in% dbb$bug_id) |>
+        filter(added %in% c("CONFIRMED", "ASSIGNED")) |>
+        group_by(bug_id) |>
+        summarize(takes_patches = (today-max(as.Date(bug_when))) > 15)
+    dbb <- merge(dbb, tp, by = "bug_id", all = TRUE, sort = FALSE) |>
+        mutate(takes_patches = if_else(is.na(takes_patches), FALSE, takes_patches))
     dbDisconnect(db_bugzilla)
-    dbb <- dbb %>% mutate(creation_ts = as.POSIXct(creation_ts, tz = "UTC", format = "%Y-%m-%d %H:%M:%OS"),
-                          delta_ts = as.POSIXct(delta_ts, tz = "UTC", format = "%Y-%m-%d %H:%M:%OS")) %>%
-        rowwise() %>%
-        mutate(last_changed = max(creation_ts, delta_ts, na.rm = TRUE)) %>%
-        ungroup() %>%
+    dbb <- mutate(dbb,
+                  creation_ts = as.POSIXct(creation_ts, tz = "UTC", format = "%Y-%m-%d %H:%M:%OS"),
+                  delta_ts = as.POSIXct(delta_ts, tz = "UTC", format = "%Y-%m-%d %H:%M:%OS")) |>
+        rowwise() |>
+        mutate(last_changed = max(creation_ts, delta_ts, na.rm = TRUE)) |>
+        ungroup() |>
         mutate(component_id = component_names[as.character(component_id)],
                opened = format(creation_ts, "%Y-%m-%d"),
-               last_changed = format(last_changed, "%Y-%m-%d %H:%M")) %>%
+               last_changed = format(last_changed, "%Y-%m-%d %H:%M")) |>
         select(-creation_ts, -delta_ts)
     as.data.frame(dbb)
 }
@@ -91,10 +100,10 @@ link_web <- function(x, id = NULL) {
 
 plot_component_core <- function(bugs){
     library("ggplot2")
-    bugs %>%
-        group_by(r_core, component_id) %>%
-        count() %>%
-        ungroup() %>%
+    bugs |>
+        group_by(r_core, component_id) |>
+        count() |>
+        ungroup() |>
         ggplot() +
         geom_tile(aes(r_core, component_id, fill = n)) +
         scale_fill_viridis_c() +
@@ -105,10 +114,10 @@ plot_component_core <- function(bugs){
 
 plot_status_version <- function(bugs){
     library("ggplot2")
-    bugs %>%
-        group_by(bug_status, version) %>%
-        count() %>%
-        ungroup() %>%
+    bugs |>
+        group_by(bug_status, version) |>
+        count() |>
+        ungroup() |>
         ggplot() +
         geom_tile(aes(bug_status, version, fill = n)) +
         scale_fill_viridis_c() +
@@ -118,10 +127,10 @@ plot_status_version <- function(bugs){
 
 plot_system_attachment <- function(bugs){
     library("ggplot2")
-    bugs %>%
-        group_by(op_sys, has_attachment) %>%
-        count() %>%
-        ungroup() %>%
+    bugs |>
+        group_by(op_sys, has_attachment) |>
+        count() |>
+        ungroup() |>
         ggplot() +
         geom_tile(aes(has_attachment, op_sys, fill = n)) +
         scale_fill_viridis_c() +
--- app.R
+++ app.R
@@ -3,24 +3,34 @@ library(shiny)
 last_dump <- "2021-03-25"
 
 ui <- fluidPage(
-    "Last time this was updated was:",
-    last_dump,
-    "\n",
+    "Open bugs on ", last_dump, "\n",
+
     dateRangeInput("period", "Bugs within these dates",
                    start = "2012-07-22",
                    min = "2012-07-22",
                    end = last_dump,
                    max = last_dump,
                    weekstart = 1),
+    fluidRow(
+        splitLayout(plotOutput("cc"),
+                    plotOutput("sv"),
+                    plotOutput("sa"),
+                    cellWidths = c("30%", "30%", "40%"))
+    ),
     dataTableOutput("bugs")
 )
 
 server <- function(input, output, session) {
-    bugs <- open_bugs()
-    bugs$bug_id <- link_web(bugs$bug_id)
-
-    output$bugs <- renderDataTable(bugs, options = list(pageLength = 20),
-                                   escape = FALSE)
+    bugs <- open_bugs() |>
+        arrange(bug_id) |>
+        mutate(bug_id = link_web(bug_id))
+    output$cc <- renderPlot(plot_component_core(bugs),
+                            alt = "Tile plot with components and if R core has answered.")
+    output$sv <- renderPlot(plot_status_version(bugs),
+                            alt = "Tile plot with status and the version of R.")
+    output$sa <- renderPlot(plot_system_attachment(bugs),
+                            alt = "Tile plot with the OS used and if it has attachments.")
+    output$bugs <- renderDataTable(bugs, options = list(pageLength = 20), escape = -1)
 }
 
 
